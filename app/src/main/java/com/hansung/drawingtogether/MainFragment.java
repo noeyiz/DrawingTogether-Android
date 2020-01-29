280b4144737b91a88df2508d88e44232216ae01f
trycatch <trycatch98@gmail.com>
2020-01-29 17:03:59 +0900

    initial commit

---
+++ app/src/main/java/com/hansung/drawingtogether/MainFragment.java
@@ -0,0 +1,23 @@
+package com.hansung.drawingtogether;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.hansung.drawingtogether.databinding.FragmentMainBinding;
+
+
+public class MainFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        FragmentMainBinding binding = FragmentMainBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+}
