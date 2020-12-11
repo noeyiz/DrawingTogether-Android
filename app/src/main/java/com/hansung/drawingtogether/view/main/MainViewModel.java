e20307d2c8bafb4791b933a1066714db949902d1
minj <minj5683@gmail.com>
2020-12-11 18:11:35 +0900

    change broker ip

--- app/src/main/java/com/hansung/drawingtogether/view/main/MainViewModel.java
+++ app/src/main/java/com/hansung/drawingtogether/view/main/MainViewModel.java
@@ -59,8 +59,8 @@ public class MainViewModel extends BaseViewModel {
 
 
         /* 변수 초기화 */
-//        ip.setValue("54.180.154.63"); // 클라우드
-        ip.setValue("54.180.55.246"); // 클라우드 [나연]
+        ip.setValue("183.111.206.208"); // h2o broker
+//        ip.setValue("54.180.55.246"); // 클라우드 [나연]
 //        ip.setValue("192.168.0.36"); // 모니터링
         port.postValue("1883");
         setTopic("");
